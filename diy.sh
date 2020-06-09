b13cd6841143e0f032af222310965e5e309b29e0
BFG <cacaocn@live.com>
2020-06-09 14:14:47 +0800

    Update

--- diy.sh
+++ diy.sh
@@ -8,5 +8,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash.git OpenClash
+cd OpenClash
 git checkout master
+cd ..
 mv OpenClash/luci-app-openclash package/
